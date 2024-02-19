b90e6b0d6ddd355df1fd2c720da4a9538d1f44c7
marble <git@computer-in.love>
2024-02-19 22:21:46 +0100

    adc: add brackets to defines

--- firmware/main/hal/adc.c
+++ firmware/main/hal/adc.c
@@ -18,8 +18,8 @@
 
 #include <stdlib.h>
 
-#define DEFAULT_VREF 1100 // Use adc2_vref_to_gpio() to obtain a better estimate
-#define NO_OF_SAMPLES 64 // Multisampling
+#define DEFAULT_VREF (1100) // Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES (64) // Multisampling
 
 static esp_adc_cal_characteristics_t* adc_chars;
 #if CONFIG_IDF_TARGET_ESP32
